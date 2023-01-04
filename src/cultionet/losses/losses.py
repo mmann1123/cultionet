c61842a5332aaf2464dc29012dbccf3d62230dfe
jgrss <jbgraesser@gmail.com>
2023-01-04 11:44:40 -0600

    added license

--- src/cultionet/losses/losses.py
+++ src/cultionet/losses/losses.py
@@ -135,13 +135,35 @@ class TanimotoComplementLoss(torch.nn.Module):
 class TanimotoDistLoss(torch.nn.Module):
     """Tanimoto distance loss
 
-    Reference:
+    References:
+        https://github.com/feevos/resuneta/blob/145be5519ee4bec9a8cce9e887808b8df011f520/nn/loss/loss.py
+
+            CSIRO BSTD/MIT LICENSE
+
+            Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+            the following conditions are met:
+
+            1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+                following disclaimer.
+            2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+                the following disclaimer in the documentation and/or other materials provided with the distribution.
+            3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+                promote products derived from this software without specific prior written permission.
+
+            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+            INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+            DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+            SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+            WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+            USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
         https://github.com/sentinel-hub/eo-flow/blob/master/eoflow/models/losses.py
 
-    MIT License
+            MIT License
 
-    Copyright (c) 2017-2020 Matej Aleksandrov, Matej Batič, Matic Lubej, Grega Milčinski (Sinergise)
-    Copyright (c) 2017-2020 Devis Peressutti, Jernej Puc, Anže Zupanc, Lojze Žust, Jovan Višnjić (Sinergise)
+            Copyright (c) 2017-2020 Matej Aleksandrov, Matej Batič, Matic Lubej, Grega Milčinski (Sinergise)
+            Copyright (c) 2017-2020 Devis Peressutti, Jernej Puc, Anže Zupanc, Lojze Žust, Jovan Višnjić (Sinergise)
     """
     def __init__(
         self,
